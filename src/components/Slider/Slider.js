098a21b53076c682bfed0dec2c1708a074579043
sakib12345 <sakib35-1680@diu.edu.bd>
2022-02-20 21:17:12 +0600

    arrow right left check

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,24 +4,35 @@ import { ArrowLeft, ArrowRight } from "@mui/icons-material";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 80vh;
   display: flex;
+  position: relative;
+  background-color: orange;
 `;
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
   background-color: #fff7f7;
-  border-radius: 50%
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position:absolute;
+  top:0;
+  bottom:0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin:auto;
 `;
 
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeft />
       </Arrow>
-      <Arrow>
-      <ArrowRight />
+      <Arrow direction="right">
+        <ArrowRight />
       </Arrow>
     </Container>
   );
