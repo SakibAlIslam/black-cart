fd0d0c22c28b0a44d218b6924fcac62435085c0d
sakib12345 <sakib35-1680@diu.edu.bd>
2022-02-20 20:49:09 +0600

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import logo from './logo/blackcartLogo.png'
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
     height: 60px;
@@ -10,6 +13,7 @@ const Wrapper = styled.div`
     padding: 10px 20px;
     display:flex;
     justify-content: space-between;
+    align-items: center;
 `;
 const Left = styled.div`
     flex:1;
@@ -22,16 +26,38 @@ const Language = styled.span`
     cursor:pointer;
 `
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
     display:flex;
     align-items: center;
+    margin-left: 25px;
+    padding: 5px
 `
-
+const Input = styled.input`
+    border: none;
+`
 const Center = styled.div`
-    flex:1
+    flex:1;
+    display:flex;
+    align-items: center;
+    justify-content: center;
 `;
+
+const Logo = styled.img.attrs({
+    src: `${logo}`
+})`
+  width: 40%;
+
+  `;
+
 const Right = styled.div`
-    flex:1
+    flex:1;
+    display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+const MenuLink = styled.div`
+    font-size: 14px;
+    cursor:pointer
 `;
 
 const Navbar = () => {
@@ -39,13 +65,21 @@ const Navbar = () => {
         <Container>
             <Wrapper>
                 <Left><Language>EN</Language>
-                <SearchContainer>
-                    Input
-                    <SearchOutlinedIcon />
-                </SearchContainer>
+                    <SearchContainer>
+                        <Input />
+                        <SearchOutlinedIcon style={{color:'gray', fontSize:'16px'}}/>
+                    </SearchContainer>
                 </Left>
-                <Center>Left</Center>
-                <Right>Left</Right>
+                <Center><Logo /></Center>
+                <Right>
+                    <MenuLink>Register</MenuLink>
+                    <MenuLink>Log In</MenuLink>
+                    <MenuLink>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlinedIcon color="action" />
+                        </Badge>
+                    </MenuLink>
+                </Right>
             </Wrapper>
         </Container>
     )
