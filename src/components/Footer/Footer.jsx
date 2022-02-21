007d20dc91b0859dbd841a4c59a8468c120debe2
sakib12345 <sakib35-1680@diu.edu.bd>
2022-02-21 16:42:19 +0600

    left done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
+import { Facebook, Instagram, LinkedIn, Pinterest, Twitter, WhatsApp } from '@mui/icons-material';
 import React from 'react'
 import styled from 'styled-components'
+import logo from '../Navbar/logo/blackcartLogo.png'
 
 const Container = styled.div`
     display:flex;
 `;
 const Left = styled.div`
-    flex:1
+    flex:1;
+    display:flex;
+    flex-direction:column;
+    padding: 30px
+`;
+
+const Logo = styled.img`
+    width:50%;
+`;
+const Desc = styled.p`
+    font-size:20px;
+    padding: 10px 0px
+`;
+const SocialContainer = styled.div`
+    display:flex;
+    justify-content: space-around
+`;
+const SocialIcon = styled.div`
+    cursor:pointer;
 `;
 const Center = styled.div`
     flex:1
@@ -17,13 +37,38 @@ const Right = styled.div`
 `;
 
 const Footer = () => {
-  return (
-    <Container>
-        <Left></Left>
-        <Center></Center>
-        <Right></Right>
-    </Container>
-  )
+    return (
+        <Container>
+            <Left>
+                <Logo src={logo} />
+                <Desc>
+                    Black Cart is the buying and selling of goods and services over the Internet. It is conducted over computers, tablets, smartphones, and other smart devices.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon>
+                        <Facebook sx={{ fontSize: '35px', color: '#3b5998' }} />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Instagram sx={{ fontSize: '35px', color: '#bc2a8d' }} />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Twitter sx={{ fontSize: '35px', color: '#00acee' }} />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Pinterest sx={{ fontSize: '35px', color: '#E60023' }} />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <LinkedIn sx={{ fontSize: '35px', color: '#0077b5' }} />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <WhatsApp sx={{ fontSize: '35px', color: '#4FCE5D' }} />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center></Center>
+            <Right></Right>
+        </Container>
+    )
 }
 
 export default Footer
