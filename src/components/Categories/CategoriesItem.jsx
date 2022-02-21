62ddde0c976320dfb0dfe0621011ea74203fa6a2
sakib12345 <sakib35-1680@diu.edu.bd>
2022-02-21 14:28:41 +0600

    categories two component ready

---
+++ src/components/Categories/CategoriesItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+const CategoriesItem = () => {
+  return (
+    <Container>categories</Container>
+  )
+}
+
+export default CategoriesItem
