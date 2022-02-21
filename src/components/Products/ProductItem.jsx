a699daf7ffdcaccac2a3ca64bf9feb3d8bdf5b8f
sakib12345 <sakib35-1680@diu.edu.bd>
2022-02-21 15:14:52 +0600

    icon added

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Container = styled.div`
 flex:1;
-margin:8px;
+margin:5px;
 height: 75vh;
 position:relative;
+`
+const Circle = styled.div`
+
+`
+const Image = styled.img`
+
+`
+const Info = styled.div`
+
+`
+const Icon = styled.div`
+
 `
 
 const ProductItem = ({item}) => {
   return (
-    <Container>ProductItem</Container>
+    <Container>
+        <Circle/>
+        <Image src={item?.img} />
+        <Info>
+            <Icon>
+                <ShoppingCartOutlinedIcon color="action" />
+            </Icon>
+            <Icon>
+                <SearchIcon color="action" />
+            </Icon>
+            <Icon>
+                <FavoriteBorderIcon color="action" />
+            </Icon>
+        </Info>
+    </Container>
   )
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,16 +4,16 @@ import { popularProducts } from '../../fakeData/data';
 import ProductItem from './ProductItem';
 
 const Container = styled.div`
-display:flex;
-padding:20px;
-justify-content:space-between;
+    display:flex;
+    padding:20px;
+    justify-content:space-between;
 `;
 
 const Products = () => {
     return (
         <Container>
             {popularProducts?.map(item => (
-                <ProductItem item={item} key={item?.id}/>
+                <ProductItem item={item} key={item?.id} />
             ))}
         </Container>
     )
